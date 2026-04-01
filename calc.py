a26f0c54dfc1ddad057589adcf0281b5a4271775
NinjaH2-2023 <dakshesh.dutt2919@gmail.com>
2026-04-01 19:19:50 +1100

    BAsic calc thing in python way

--- calc.py
+++ calc.py
@@ -1,1 +1,17 @@
-print('Hello from Dakshesh!')
+def compute(num1, operator, num2):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        print('unknown operator!')
+        return None
+
+print(compute(10, '+', 5))
+print(compute(10, '-', 5))
+print(compute(10, '*', 5))
+print(compute(10, '/', 5))
