b5cfc537e479528c2ea2b331fb23bd895e7985a3
NinjaH2-2023 <dakshesh.dutt2919@gmail.com>
2026-04-01 19:46:15 +1100

    Just changed a bit the code was not working

--- calc.py
+++ calc.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-#  Calculator - FIT3047 Team 123
+# Basic Calculator - FIT3047 Team 123
 
-=======
 print('Hello from Dakshesh!')
-print('hello Zihao')
->>>>>>> 7f9dcaaadcdec050c86925cc0f12615ca2113d21
+print('Hello from Zihao!')
+
 def compute(num1, operator, num2):
     if operator == '+':
         return num1 + num2
@@ -17,15 +15,8 @@ def compute(num1, operator, num2):
     else:
         print('unknown operator!')
         return None
-# Power function
-def power(num1, num2):
-    return num1 ** num2
 
-<<<<<<< HEAD
-print(power(2, 3))
-=======
 print(compute(10, '+', 5))
 print(compute(10, '-', 5))
 print(compute(10, '*', 5))
 print(compute(10, '/', 5))
->>>>>>> 7f9dcaaadcdec050c86925cc0f12615ca2113d21
